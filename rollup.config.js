a1c409003272e0402a1ef7369a7704e9d49d336b
Henrik Drefs <henrik.drefs@outlook.de>
2021-02-20 23:53:42 +0100

    upgraded dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -6,6 +6,7 @@ import { terser } from 'rollup-plugin-terser';
 import replace from '@rollup/plugin-replace';
 import { config } from 'dotenv';
 import * as path from 'path';
+import css from 'rollup-plugin-css-only';
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -26,15 +27,15 @@ export default {
       }),
     }),
     svelte({
-      // enable run-time checks when not in production
+      compilerOptions: {
+              // enable run-time checks when not in production
       dev: !production,
-      // we'll extract any component CSS out into
-      // a separate file - better for performance
-      css: (css) => {
-        css.write('public/build/bundle.css');
+        hydratable: true,
       },
-      hydratable: true,
     }),
+         // we'll extract any component CSS out into
+      // a separate file - better for performance
+    css({ output: 'bundle.css'}),
     resolve({
       browser: true,
       dedupe: (importee) =>
